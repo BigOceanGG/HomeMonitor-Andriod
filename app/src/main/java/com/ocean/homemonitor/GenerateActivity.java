0214aa2782e559a3a2a6a0aa8ed50d2884432641
b91foundation <b91foundation>
2019-08-02 14:14:55 +0800

    generate

--- app/src/main/java/com/ocean/homemonitor/GenerateActivity.java
+++ app/src/main/java/com/ocean/homemonitor/GenerateActivity.java
@@ -3,13 +3,17 @@ package com.ocean.homemonitor;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class GenerateActivity extends BaseActivity {
+public class GenerateActivity extends BaseActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
+
+        findViewById(R.id.btn_create).setOnClickListener(this);
+        findViewById(R.id.btn_login).setOnClickListener(this);
     }
 
     public static void launch(Activity from, boolean closeOther) {
@@ -23,6 +27,17 @@ public class GenerateActivity extends BaseActivity {
     public static void launch(Activity from) {
         Intent intent = new Intent(from, GenerateActivity.class);
         from.startActivity(intent);
-//        openAlpha(from);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_create:
+
+                break;
+            case R.id.btn_login:
+
+                break;
+        }
     }
 }
